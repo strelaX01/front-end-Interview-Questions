0526a78962a297aa56c90da6ea9ccc2a3a6ca3e9
strelaX01 <hoanggiangstrela@gmail.com>
2024-03-05 05:57:10 +0700

    demo

---
+++ testcase/src/main/java/demo/testcase/App.java
@@ -0,0 +1,13 @@
+package demo.testcase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
